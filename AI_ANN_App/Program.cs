80997bc1efe1d05389877c436c88e0aa735ba178
Noah Schultz <noahgschultz@gmail.com>
2023-11-15 19:30:10 -0500

    Put everything into it's own functions because

--- AI_ANN_App/Program.cs
+++ AI_ANN_App/Program.cs
@@ -51,10 +51,7 @@ class Program
         List<double[]> testList = GetListFromFile("optdigits_test.txt");
         List<double[]> trainList = GetListFromFile("optdigits_train.txt");
 
-        int numCorrect;
-        List<double> correctPercentages = new List<double>();
         int maxGenerations = 20;
-
         var watch = Stopwatch.StartNew();
 
         // Loop through g generations
@@ -62,7 +59,6 @@ class Program
         {
             // Print generation num
             Console.WriteLine("GENERATION - " + (g + 1));
-            numCorrect = 0;
 
             // Define hidden layer sizes
             int[] hiddenLayers = new int[] {30, 15};
@@ -70,49 +66,97 @@ class Program
             // Creating a neural network with 64 inputs, 2 hidden layers, and 2 outputs
             NeuralNetwork myNetwork = new NeuralNetwork(64, hiddenLayers, 10);
 
-            // Train
-            for (int h = 0; h < trainList.Count; h++) 
-            {
-                // put the input from the list into an array to pass to net
-                double[] input = new double[64];
-                for (int i = 0; i < 64; i++)
-                {
-                    input[i] = trainList[h][i];
-                }
+            TrainData(trainList, myNetwork);
 
-                // Set the current answer
-                double answer = trainList[h][64];
-
-                // Process the input through the network
-                double[] output = myNetwork.FeedForward(input, answer);
-
-                double bestAnswer = 0.0;
-                int bestAnswerIndex = 0;
-
-                for (int i = 0; i < output.Length; i++)
-                {
-                    if (output[i] > bestAnswer) 
-                    {
-                        bestAnswer = output[i];
-                        bestAnswerIndex = i;
-                    }
-                }
-
-                if (bestAnswerIndex == answer)
-                {
-                    //Console.WriteLine("Best Answer: " + bestAnswerIndex + " - Real Answer: " + answer + " - CORRECT");
-                    numCorrect++;
-                }
-            }
-            
-            double percent = (double)trainList.Count / (double)numCorrect;
-
-            Console.WriteLine("Num Correct: " + numCorrect + " - " + percent.ToString("n2") + "%");
-            correctPercentages.Add((double)trainList.Count / (double)numCorrect);
+            // TestData(testList, myNetwork);
         }
 
         watch.Stop();
         Console.WriteLine("Elapsed Time: " + watch.ElapsedMilliseconds);    
+    }
+
+    public static void TrainData (List<double[]> trainList, NeuralNetwork myNetwork) 
+    {
+        int numCorrect = 0;
+        List<double> correctPercentages = new List<double>();
+        // Train
+        for (int h = 0; h < trainList.Count; h++) 
+        {
+            // put the input from the list into an array to pass to net
+            double[] input = new double[64];
+            for (int i = 0; i < 64; i++)
+            {
+                input[i] = trainList[h][i];
+            }
+
+            // Set the current answer
+            double answer = trainList[h][64];
+
+            // Process the input through the network
+            double[] output = myNetwork.FeedForward(input, answer);
+
+            double bestAnswer = 0.0;
+            int bestAnswerIndex = 0;
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                if (output[i] > bestAnswer) 
+                {
+                    bestAnswer = output[i];
+                    bestAnswerIndex = i;
+                }
+            }
+
+            if (bestAnswerIndex == answer)
+            {
+                //Console.WriteLine("Best Answer: " + bestAnswerIndex + " - Real Answer: " + answer + " - CORRECT");
+                numCorrect++;
+            }
+        }
+        
+        double percent = (double)trainList.Count / (double)numCorrect;
+
+        Console.WriteLine("Num Correct: " + numCorrect + " - " + percent.ToString("n2") + "%");
+        correctPercentages.Add((double)trainList.Count / (double)numCorrect);
+    }
+
+    public static void TestData(List<double[]> testList, NeuralNetwork myNetwork) 
+    {
+        int numCorrect = 0;
+        // TEST
+        for (int h = 0; h < testList.Count; h++) 
+        {
+            // put the input from the list into an array to pass to net
+            double[] input = new double[64];
+            for (int i = 0; i < 64; i++)
+            {
+                input[i] = testList[h][i];
+            }
+
+            // Set the current answer
+            double answer = testList[h][64];
+
+            // Process the input through the network
+            double[] output = myNetwork.FeedForward(input, answer);
+
+            double bestAnswer = 0.0;
+            int bestAnswerIndex = 0;
+
+            for (int i = 0; i < output.Length; i++)
+            {
+                if (output[i] > bestAnswer) 
+                {
+                    bestAnswer = output[i];
+                    bestAnswerIndex = i;
+                }
+            }
+
+            if (bestAnswerIndex == answer)
+            {
+                //Console.WriteLine("Best Answer: " + bestAnswerIndex + " - Real Answer: " + answer + " - CORRECT");
+                numCorrect++;
+            }
+        }
     }
 }
 
